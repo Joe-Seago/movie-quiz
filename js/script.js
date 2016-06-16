d062c024f00d59dac6ec169752255e4d5a68707a
Joe Seago <joe.seago@gmail.com>
2016-06-16 10:48:44 -0600

    creating q+a object

--- js/script.js
+++ js/script.js
@@ -20,15 +20,22 @@
 $(document).ready(function() {
 
 	var movieQuestions = [{
-		question: 'What film is Maximus from?',
+		question: 'What film is the character Maximus from?',
 		answer: ['Matrix', 'Toy Story', 'Gladiator', 'Lord of the Rings']
 	}, {
-		question: 'Who is "The One" in The Matrix?',
+		question: 'Which character is "The One" in The Matrix?',
 		answer: ['Morpheus', 'Agent Smith', 'Maximus', 'Neo']
-	}, 
-	{}, 
-	{}, 
-	{}]
+	}, {
+		question: 'Which character is NOT in Toy Story?',
+		answer: ['Buzz Lightyear', 'Fuzzy', 'Woody', 'Slinky']
+
+	}, {
+		question: 'Which actor played the character Luke Skywalker in Star Wars?',
+		answer: ['Harrison Ford', 'James Earl Jones', 'Mark Hamill', 'Steven Seagal']
+	}, {
+		question: 'What street did the character Rachel Dawes die on in Batman: Dark Knight?',
+		answer: ['Avenue X at Cicero', '1007 Mountain Drive', '250 52nd Street', '401 North Cicero Avenue']
+	}];
 
 
 
