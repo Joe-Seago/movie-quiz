20d932b46f92fc2d7085b8758379c454b78d36c9
Sean Jun <seanjun21@gmail.com>
2016-06-16 09:32:58 -0700

    HTML, pseudocodes, scripts done

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Requires the user to answer at least five questions
+
+// Shows each question one at a time
+
+// Tells the user each question's number and how many questions remain
+
+// Prevents the user from skipping questions
+
+// Gives the user some way to answer each question
+
+// Compares the user's answer, and the correct answer to determine a score
+
+// Displays the user's final score once all questions have been answered
+
+// Allows the user to start a new game once all questions have been answered
+
+// Uses objects to represent the questions and answers
+
+
+$(document).ready(function() {
+
+	var movieQuestions = [{
+		question: 'What film is Maximus from?',
+		answer: ['Matrix', 'Toy Story', 'Gladiator', 'Lord of the Rings']
+	}, {
+		question: 'Who is "The One" in The Matrix?',
+		answer: ['Morpheus', 'Agent Smith', 'Maximus', 'Neo']
+	}, 
+	{}, 
+	{}, 
+	{}]
+
+
+
+})
