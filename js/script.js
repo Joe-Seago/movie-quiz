5641d2e7777fa7f86c43b2741a68517aef96014c
Sean Jun <seanjun21@gmail.com>
2016-06-16 14:19:38 -0700

    added question tracker and other features

--- js/script.js
+++ js/script.js
@@ -17,68 +17,83 @@
 // Uses objects to represent the questions and answers
 
 var score = 0;
+var qN = 0;
 
 $(document).ready(function() {
 
-	var movieQuestions = [
-		// Question 1
+	var movieQs = [
 		{
 			question: 'What film is the character Maximus from?',
 			answers: ['Matrix', 'Toy Story', 'Gladiator', 'Lord of the Rings'],
-			correct: 'Gladiator'
+			correct: 'Gladiator',
+			number: 1
 		}, 
-		// Question 2
 		{
 			question: 'Which character is "The One" in The Matrix?',
 			answers: ['Morpheus', 'Agent Smith', 'Maximus', 'Neo'],
-			correct: 'Neo'
+			correct: 'Neo',
+			number: 2
 		}, 
-		// Question 3
 		{
 			question: 'Which character is NOT in Toy Story?',
 			answers: ['Buzz Lightyear', 'Fuzzy', 'Woody', 'Slinky'],
-			correct: 'Fuzzy'
+			correct: 'Fuzzy',
+			number: 3
 		}, 
-		// Question 4
 		{
 			question: 'Which actor played the character Luke Skywalker in Star Wars?',
 			answers: ['Harrison Ford', 'James Earl Jones', 'Mark Hamill', 'Steven Seagal'],
-			correct: 'Mark Hamill'
+			correct: 'Mark Hamill',
+			number: 4
 		}, 
-		// Question 5
 		{
 			question: 'What street did the character Rachel Dawes die on in Batman: Dark Knight?',
 			answers: ['Avenue X at Cicero', '1007 Mountain Drive', '250 52nd Street', '401 North Cicero Avenue'],
-			correct: '250 52nd Street'
+			correct: '250 52nd Street',
+			number: 5,
 		}
 	];
 
-	$('.query').append('<p>' + movieQuestions[0].question + '</p>');
 
-	for (var i = 0; i < movieQuestions[0].answers.length; i++) {
-		console.log(movieQuestions[0].answers[i])
-		$('.choices ul').append('<li><input type = "radio" name = "radioSize" value = "' + movieQuestions[0].answers[i] + '">' + movieQuestions[0].answers[i] + '</li>');
-	}
+	movieLoop(movieQs);
 
 	// Upon clicking submit
 	$('.submitButton').click(function() {
 		// If our choice value is correct
-		if ($('input[name="radioSize"]:checked').val() === movieQuestions[0].correct) {
+		if ($('input[name="radioSize"]:checked').val() === movieQs[qN].correct) {
 			score++;
 			console.log("right!");
+			console.log(movieQs[qN].correct);
 		} else {
 			console.log("you suck!");
 		}
-			//Add to the score
-		
-
 			// Move to next question
-			
+
+		qN++;
+		if (qN > 4) {
+			$('.numberBoard').text("You got " + score + " points total");
+			$('.submit').hide();
+			$('.quiz').hide();
+		} else {
+
+			$('.choices ul').empty();
+
+			movieLoop(movieQs);
+		}
+
+
 	})
-		
-
-		
-
 
 })
+
+function movieLoop (movieQs) {
+	$('.query').text(movieQs[qN].question);
+	for (var i = 0; i < movieQs[qN].answers.length; i++) {
+		$('.choices ul').append('<li><input type = "radio" name = "radioSize" value = "' + movieQs[qN].answers[i] + '">' + movieQs[qN].answers[i] + '</li>');
+		$('.numberBoard').html('<p>You are on question no. ' + movieQs[qN].number + '</p>');
+		$('.numberBoard').append(movieQs[movieQs.length - 1].number - movieQs[qN].number + ' questions left');
+	}
+
+}
+
 
